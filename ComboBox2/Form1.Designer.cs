b41ebe4599c4f177c6f97a1ecfe5f62074999c6e
João Pedro <112526011+JohnPdro@users.noreply.github.com>
2023-03-08 22:04:10 -0300

    Adicionar arquivos de projeto.

---
+++ ComboBox2/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace ComboBox2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textNome = new System.Windows.Forms.TextBox();
+            this.cmbSelecao = new System.Windows.Forms.ComboBox();
+            this.btnRemover = new System.Windows.Forms.Button();
+            this.btnAdicionar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(11, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Digite seu Nome";
+            // 
+            // textNome
+            // 
+            this.textNome.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textNome.Location = new System.Drawing.Point(113, 31);
+            this.textNome.Multiline = true;
+            this.textNome.Name = "textNome";
+            this.textNome.Size = new System.Drawing.Size(238, 23);
+            this.textNome.TabIndex = 1;
+            this.textNome.TextChanged += new System.EventHandler(this.textNome_TextChanged);
+            // 
+            // cmbSelecao
+            // 
+            this.cmbSelecao.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbSelecao.FormattingEnabled = true;
+            this.cmbSelecao.Location = new System.Drawing.Point(113, 73);
+            this.cmbSelecao.Name = "cmbSelecao";
+            this.cmbSelecao.Size = new System.Drawing.Size(121, 24);
+            this.cmbSelecao.TabIndex = 2;
+            this.cmbSelecao.SelectedIndexChanged += new System.EventHandler(this.cmbSelecao_SelectedIndexChanged);
+            // 
+            // btnRemover
+            // 
+            this.btnRemover.BackColor = System.Drawing.Color.MintCream;
+            this.btnRemover.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRemover.Location = new System.Drawing.Point(265, 138);
+            this.btnRemover.Name = "btnRemover";
+            this.btnRemover.Size = new System.Drawing.Size(86, 37);
+            this.btnRemover.TabIndex = 4;
+            this.btnRemover.Text = "Remover";
+            this.btnRemover.UseVisualStyleBackColor = false;
+            this.btnRemover.Click += new System.EventHandler(this.btnRemover_Click);
+            // 
+            // btnAdicionar
+            // 
+            this.btnAdicionar.BackColor = System.Drawing.Color.MintCream;
+            this.btnAdicionar.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdicionar.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnAdicionar.Location = new System.Drawing.Point(113, 138);
+            this.btnAdicionar.Name = "btnAdicionar";
+            this.btnAdicionar.Size = new System.Drawing.Size(86, 37);
+            this.btnAdicionar.TabIndex = 5;
+            this.btnAdicionar.Text = "Adicionar";
+            this.btnAdicionar.UseVisualStyleBackColor = false;
+            this.btnAdicionar.Click += new System.EventHandler(this.btnAdicionar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightCyan;
+            this.ClientSize = new System.Drawing.Size(374, 197);
+            this.Controls.Add(this.btnAdicionar);
+            this.Controls.Add(this.btnRemover);
+            this.Controls.Add(this.cmbSelecao);
+            this.Controls.Add(this.textNome);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textNome;
+        private System.Windows.Forms.ComboBox cmbSelecao;
+        private System.Windows.Forms.Button btnRemover;
+        private System.Windows.Forms.Button btnAdicionar;
+    }
+}
+
---
+++ ComboBox2/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComboBox2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdicionar_Click(object sender, EventArgs e)
+        {
+
+            if (textNome.TextLength == 0)
+                MessageBox.Show("Por favor digite algo", "AVISO", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            textNome.Focus();
+
+            {
+                cmbSelecao.Items.Add(textNome.Text);
+                textNome.Clear();
+                textNome.Focus();
+            }
+        }
+
+        private void cmbSelecao_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbSelecao.SelectedIndex = 0;
+        }
+
+        private void btnRemover_Click(object sender, EventArgs e)
+        {
+            if (cmbSelecao.SelectedIndex == 0)
+                MessageBox.Show("Por favor digite algo", "AVISO", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            textNome.Focus();
+
+            {
+                cmbSelecao.Items.Remove(cmbSelecao.SelectedItem);
+                cmbSelecao.SelectedIndex = 0;
+            }
+        }
+
+        private void textNome_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
